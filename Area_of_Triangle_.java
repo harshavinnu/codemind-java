55dbce111e3df9b26889a8132f20100c03d0fe30
harshavinnu <119720338+harshavinnu@users.noreply.github.com>
2023-05-09 20:31:18 +0530

    import java.util.Scanner; public class Codemind {     public static void main(String args[])     {         int a,b,c;         float s,n;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         c=sc.nextInt();         s=(float)(a+b+c)/2;         n=s*(s-a)*(s-b)*(s-c);         System.out.printf("%.2f",Math.sqrt(n));         sc.close();         }     }

---
+++ Area_of_Triangle_.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Codemind
+{ 
+    public static void main(String args[]) 
+    {
+        int a,b,c;
+        float s,n;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        s=(float)(a+b+c)/2;
+        n=s*(s-a)*(s-b)*(s-c);
+        System.out.printf("%.2f",Math.sqrt(n));
+        sc.close();
+        }
+    }
